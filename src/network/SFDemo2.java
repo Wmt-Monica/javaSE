b47f2fc9dd74edde59794cb44a0956aa481ebfa0
Wmt-Monica <3040988158@qq.com>
2020-11-12 11:42:47 +0800

    2020/11/12 /**  * 模拟登录 多客户请求  * 客户端：SF  */

---
+++ src/network/SFDemo2.java
@@ -0,0 +1,18 @@
+package network;
+
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * 模拟登录 多客户请求
+ * 客户端：SF
+ */
+public class SFDemo2 {
+    public static void main(String[] args) throws IOException {
+        //创建客户端
+        Socket clientSocket = null;
+        int clientPort = 8888;
+        TCPClientDemo6 client = new TCPClientDemo6(clientSocket,clientPort);
+        client.TCPClient();
+    }
+}
